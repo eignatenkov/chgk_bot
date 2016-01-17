2ebc2bc0c6034ced84b16329a4dcff1039fa8bdd
Egor Ignatenkov <egor.ignatenkov@gmail.com>
2016-01-18 00:32:36 +0300

    add descriptor to work with question fields

--- bot_tools.py
+++ bot_tools.py
@@ -1,25 +1,60 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from xml_tools import q_and_a, tournament_info
+from constants import TRANSLATIONS
+from weakref import WeakKeyDictionary
 
 
-class Question:
+class XMLField(object):
+    def __init__(self, field_name, default_value=''):
+        self.field_name = field_name
+        self.default_value = default_value
+        self.data = WeakKeyDictionary()
+
+    def __set__(self, instance, question_dict):
+        self.data[instance] = question_dict.get(self.field_name, '')
+
+    def __get__(self, instance, owner):
+        if self.data[instance]:
+            return u'{0}: {1}\n'.format(TRANSLATIONS[self.field_name], self.data[instance])
+        else:
+            return ''
+
+
+class Question(object):
+    question = XMLField('question')
+    question_image = XMLField('question_image')
+    answer = XMLField('answer')
+    pass_criteria = XMLField('pass_criteria')
+    comments = XMLField('comments')
+    sources = XMLField('sources')
+    authors = XMLField('authors')
+
     def __init__(self, tournament_id, tour_number, question_number):
         """
         :param data: словарь необходимых полей: вопрос, ответ, комментарий, зачет, пр.
         :return:
         """
-        data = q_and_a(tournament_id, tour_number, question_number)
-        self.question = data.get('question', '')
-        self.question_image = data.get('question_image', '')
-        self.answer = data.get('answer', '')
-        self.pass_criteria = data.get('pass_criteria', '')
-        self.comments = data.get('comments', '')
-        self.sources = data.get('sources', '')
-        self.authors = data.get('authors')
+        question_dict = q_and_a(tournament_id, tour_number, question_number)
+        self.question = question_dict
+        self.question_image = question_dict
+        self.answer = question_dict
+        self.pass_criteria = question_dict
+        self.comments = question_dict
+        self.sources = question_dict
+        self.authors = question_dict
+
+    @property
+    def full_answer(self):
+        return u'{0}{1}{2}{3}{4}'.format(self.answer, self.pass_criteria, self.comments, self.sources, self.authors)
 
 
-class Tournament:
+class Tournament(object):
+    title = XMLField('title')
+    description = XMLField('description')
+    number_of_tours = XMLField('n_tours')
+    number_of_questions = XMLField('n_questions')
+
     def __init__(self, url):
         self.url = url
         data = tournament_info(url)
@@ -36,9 +71,11 @@ class Tournament:
     def __iter__(self):
         return self
 
-    def next(self):
+    # def next(self):
 
 if __name__ == "__main__":
     test_question = Question('har14-h2', 2, 2)
-    print test_question.question
-    print test_question.answer
+    print test_question.question, test_question.full_answer
+    test_question = Question('har14-h2', 2, 3)
+    print test_question.question, test_question.full_answer
+
---
+++ constants.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+TRANSLATIONS = {
+    'question': u'Вопрос',
+    'answer': u'Ответ',
+    'pass_criteria': u'Зачет',
+    'comments': u'Комментарии',
+    'sources': u'Источники',
+    'authors': u'Авторы'
+}
