216b42a8f4fa00d57ebe05c16c1385c4d335d70c
Egor Ignatenkov <egor.ignatenkov@gmail.com>
2019-04-22 11:53:06 +0200

    bug fix

--- rating_tools.py
+++ rating_tools.py
@@ -27,7 +27,7 @@ def get_tournament_results_by_id(tournament_id):
     url = 'http://rating.chgk.info/api/tournaments/{}/list.json'.format(
         tournament_id)
     results = api_call(url)
-    if 'position' not in results[0] and not all(int(results[i]['idteam']) <= int(results[i + 1]['idteam']) for i in range(len(results)-2)):
+    if results and 'position' not in results[0] and not all(int(results[i]['idteam']) <= int(results[i + 1]['idteam']) for i in range(len(results)-2)):
         for i in range(len(results)):
             results[i]['position'] = str(i + 1)
     return results
